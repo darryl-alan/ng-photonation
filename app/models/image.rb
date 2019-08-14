5925dd800ca6ba2d72d25a02c600d8b8061c65ee
Darryl Alan <drl.iceman@gmail.com>
2019-08-14 22:15:28 +0700

    Add imagemagick to resize images

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,14 @@
 class Image < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+
+  validate :image_size
+
+  private
+  def image_size
+    maximum_size = 7
+    if image.size > maximum_size.megabytes
+      errors.add(:picture, "maximum size is #{maximum_size} MB")
+    end
+  end
 end
